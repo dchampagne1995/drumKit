cb313fb9cfa22a6d53731b9fb660fef2df1a4e3b
dchampagne1995 <69989583+dchampagne1995@users.noreply.github.com>
2020-11-05 23:10:19 -0800

    added button flash animation function

--- index.js
+++ index.js
@@ -1,57 +1,69 @@
-
 // Detecing mouse clicks
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     buttonHtml = this.innerHTML;
     determineKey(buttonHtml);
+    buttonFlash(buttonHtml);
   });
 }
 
 //Detecing key press
-document.addEventListener("keypress", function(event){
-    determineKey(event.key);
+document.addEventListener("keypress", function(event) {
+  determineKey(event.key);
+  buttonFlash(event.key);
 });
 
-function determineKey(key){
+// Determining which key was pressed to match sound
+function determineKey(key) {
 
   switch (key) {
 
     case "w":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
-    break;
+      break;
 
     case "a":
       var kick1 = new Audio('sounds/kick-bass.mp3');
       kick1.play();
-    break;
+      break;
 
     case "s":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
-    break;
+      break;
 
     case "d":
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
-    break;
+      break;
 
     case "j":
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
-    break;
+      break;
 
     case "k":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
-    break;
+      break;
 
     case "l":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
-    break;
+      break;
 
     default:
       console.log(buttonHtml);
   }
+}
+
+// Including button flashing animation
+function buttonFlash(key) {
+  var currentBtn = document.querySelector("." + key);
+  currentBtn.classList.add("pressed");
+
+  setTimeout(function() {
+    currentBtn.classList.remove("pressed");
+  }, 100)
 }
